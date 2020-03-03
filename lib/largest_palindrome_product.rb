083e4af26e8920e00532a173ba7153c9c66b130f
mattbechtel1 <bechma02@gmail.com>
2020-03-02 20:21:28 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,33 @@
 # Implement your procedural solution here!
+
+def is_palindrome?(string_num)
+    if string_num.length < 2
+        return true
+    elsif string_num[0] === string_num[-1] 
+        is_palindrome?(string_num[1..-2])
+    else
+        return false
+    end
+end
+
+def largest_palindrome_product
+    i = 999
+    k = 999
+    stored_k = 0
+    stored_palindrome = 0
+    while i > 99 && i > stored_k do
+        while k > 99 && k > stored_k do
+            if i * k > stored_palindrome && is_palindrome?((i * k).to_s) 
+                stored_k = k
+                stored_palindrome = i * k
+                k = 999
+                i -= 1
+            else
+                k -= 1
+            end
+        end
+        k = 999
+        i -= 1
+    end
+    stored_palindrome
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+require 'largest_palindrome_product'
+
+class LargestPalindromeProduct
+
+    def initialize
+    end
+
+    def answer
+        largest_palindrome_product
+    end
+
+
+end
